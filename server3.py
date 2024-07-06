aefc5c6223b571965228e1fe16d2ef6e105beeab
DhruvGupta21 <dhruvgenie21@gmail.com>
2024-07-06 10:53:13 +0530

    changes

--- server3.py
+++ server3.py
@@ -1,3 +1,4 @@
+
 ################################################################################
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a
@@ -59,7 +60,7 @@ OVERLAP = 4
 # Test Data
 
 def bwalk(min, max, std):
-    """ Generates a bounded random walk. """
+
     rng = max - min
     while True:
         max += normalvariate(0, std)
